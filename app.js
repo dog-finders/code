6b76afab93d10877e3808e0e426912dab3ce6a66
dongwoo <woomacho@gmail.com>
2025-05-28 18:29:55 +0900

    chore:

--- app.js
+++ app.js
@@ -2,9 +2,10 @@ const express = require('express');
 const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
+const session = require('express-session');
 
 const indexRouter = require('./routes/index');
-const usersRouter = require('./backend/domain/user/routes/userRoutes');
+const userRoutes = require('./backend/domain/user/routes/userRoutes');
 const petRouters = require('./backend/domain/pet/routes/petRoutes');
 const { AppDataSource } = require('./backend/global/config/typeOrmConfig');
 
@@ -25,8 +26,37 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(
+    session({
+        secret: 'my-secret',
+        resave: false,
+        saveUninitialized: false,
+        cookie: {
+            httpOnly: true,
+            secure: false,
+            maxAge: 1000 * 60 * 60, // 1시간
+        },
+    }),
+);
 
+app.use(function (req, res, next) {
+    console.log(req.url);
+    next();
+});
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use('/api/v1/users', userRoutes);
+
+app.use(
+    session({
+        resave: false,
+        saveUninitialized: false,
+        secret: 'secret',
+        cookie: {
+            maxAge: 1000 * 60 * 60 * 24 * 7,
+            secure: false,
+            httpOnly: true,
+        },
+    }),
+);
 
 module.exports = app;
