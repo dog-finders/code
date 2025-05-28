7bcb98caad2c5691a8d13f2a61285a2d7d7dfad3
dongwoo <woomacho@gmail.com>
2025-05-28 18:29:31 +0900

    chore: pet 초기 세팅

---
+++ backend/domain/pet/entity/Pet.js
@@ -0,0 +1,26 @@
+const { EntitySchema } = require('typeorm');
+
+module.exports = new EntitySchema({
+    name: 'Pet',
+    tableName: 'pet',
+    columns: {
+        id: { type: Number, primary: true, generated: true },
+        name: { type: String, length: 50 },
+        birth: { type: 'date' },
+        personality: {
+            type: 'enum',
+            enum: ['ACTIVE', 'CALM', 'SOCIAL', 'SHY'],
+        },
+        isVaccinated: { type: Boolean },
+        createdAt: { type: 'timestamp', createDate: true },
+        updatedAt: { type: 'timestamp', updateDate: true },
+    },
+    relations: {
+        user: {
+            type: 'many-to-one',
+            target: 'User',
+            joinColumn: true,
+            onDelete: 'CASCADE',
+        },
+    },
+});
